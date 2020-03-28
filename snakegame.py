31c9f3aae66f16ff688ef47c329c7a18c70b0cfc
rstar ginni <48328833+ratankumarmaurya@users.noreply.github.com>
2020-03-28 22:19:22 +0530

    Update snakegame.py

--- snakegame.py
+++ snakegame.py
@@ -2,7 +2,7 @@ import curses
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 from random import randint
 
-
+  #RSTAR
 curses.initscr()
 win = curses.newwin(20, 60, 0, 0)
 win.keypad(1)
@@ -11,33 +11,32 @@ curses.curs_set(0)
 win.border(0)
 win.nodelay(1)
 
-key = KEY_RIGHT                                                    # Initializing values
+key = KEY_RIGHT                                                    
 score = 0
 
-snake = [[4,10], [4,9], [4,8]]                                     # Initial snake co-ordinates
-food = [10,20]                                                     # First food co-ordinates
+snake = [[4,10], [4,9], [4,8]]                                    
+food = [10,20]                                                     
 
-win.addch(food[0], food[1], 'Q')                                   # Prints the food
+win.addch(food[0], food[1], 'Q')                                   
 
-while key != 27:                                                   # While Esc key is not pressed
+while key != 27:                                                   
     win.border(0)
-    win.addstr(0, 2, 'Score : ' + str(score) + ' ')                # Printing 'Score' and
-    win.addstr(0, 27, ' SNAKE ')                                   # 'SNAKE' strings
-    win.timeout(150 - (len(snake)/5 + len(snake)/10)%120)          # Increases the speed of Snake as its length increases
+    win.addstr(0, 2, 'Score : ' + str(score) + ' ')                
+    win.addstr(0, 27, ' SNAKE ')                                   
+    win.timeout(150 - (len(snake)/5 + len(snake)/10)%120)         
     
-    prevKey = key                                                  # Previous key pressed
+    prevKey = key                                                  
     event = win.getch()
     key = key if event == -1 else event 
 
 
-    if key == ord(' '):                                            # If SPACE BAR is pressed, wait for another
-        key = -1                                                   # one (Pause/Resume)
-        while key != ord(' '):
+    if key == ord(' '):                                           
+        key = -1                                                   
             key = win.getch()
         key = prevKey
         continue
 
-    if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
+    if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     
         key = prevKey
 
     # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
@@ -57,18 +56,22 @@ while key != 27:                                                   # While Esc k
     if snake[0] in snake[1:]: break
 
     
-    if snake[0] == food:                                            # When snake eats the food
+    if snake[0] == food:                                           
         food = []
         score += 1
         while food == []:
-            food = [randint(1, 18), randint(1, 58)]                 # Calculating next food's coordinates
+            food = [randint(1, 18), randint(1, 58)]                 
             if food in snake: food = []
         win.addch(food[0], food[1], 'Q')
     else:    
-        last = snake.pop()                                          # [1] If it does not eat the food, length decreases
+        last = snake.pop()                                          
         win.addch(last[0], last[1], ' ')
     win.addch(snake[0][0], snake[0][1], '+')
     
 curses.endwin()
 print("\nScore - " + str(score))
 print("http://bitemelater.in\n")
+
+
+
+# THANKS FOR DOWNLOADS BY RSTAR
